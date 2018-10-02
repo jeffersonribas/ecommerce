56c7baa61dfdd836fe1ec446b84f496860e3fb17
Your Name <you@example.com>
2018-10-01 23:04:13 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
-
+require_once("functions.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+
 
 $app = new Slim();
 
@@ -12,15 +14,48 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page = new Page();
+	$page = new Hcode\Page();
+
 	$page->setTpl("index");
 
 });
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();
+
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('login'), post('password'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
